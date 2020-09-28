a6c31eb8d3df2bbc4dc0a8c551786780eefea50d
rohit10000 <rohitsingh18997@gmail.com>
2020-09-29 01:51:05 +0530

    Component Part 1: used bootstrap navbar navbarBrand Media props and state of component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <p> Congrats! We are ready!</p>
-    </div>
-  );
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from "./components/Menu";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu/>
+            </div>
+        );
+    }
 }
 
 export default App;
